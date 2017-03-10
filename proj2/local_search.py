2d4a7f4512159229d62b41bc8908a3c67a37dbab
linkvetern64 <jstand1@umbc.edu>
2017-03-10 04:09:38 -0500

    Simple hill climbing

--- proj2/local_search.py
+++ proj2/local_search.py
@@ -1,10 +1,59 @@
 from schedule import Schedule
+import copy
 
-def hillClimb():
+
+
+
+
+def hillClimb(file):
     print("Hill Climbing: ")
-    schedule = Schedule
-    schedule.load(schedule, "sch1.txt")
-    #hill climbing goes here.
+    schedule = tmp = Schedule(0,0)
+    schedule.load(file)
+    #For each day
+    #for each worker
+    #deep copy the schedule
+    #update each worker until you get better heuristic
+    #set schedule to new schedule created
+    #if no the schedule is no longer producing better heuristics
+    #return the schedule
+
+    # --------------
+    # Maybe implement a look ahead?
+    # --------------
+
+    tmp = copy.deepcopy(schedule)
+    print(schedule.value3())
+    for day in schedule.schedule:
+        tmp_m = day.morning
+        tmp_e = day.evening
+        tmp_g = day.graveyard
+        best = schedule.value3()
+        #assigning day a value, changes it for entire schedule
+        for worker in schedule.workers:
+            day.morning = worker
+            if schedule.value3() >= best:
+                best = schedule.value3()
+                tmp_m = worker
+        day.morning = tmp_m
+
+        for worker in schedule.workers:
+            day.evening = worker
+            if schedule.value3() >= best:
+                best = schedule.value3()
+                tmp_e = worker
+        day.evening = tmp_e
+
+        for worker in schedule.workers:
+            day.graveyard = worker
+            if schedule.value3() >= best:
+                best = schedule.value3()
+                tmp_g = worker
+        day.graveyard = tmp_g
+    return schedule
+
+                #day.morning = getBestDay(schedule, day, "morning")
+        #getBestDay(schedule, day, "evening")
+        #getBestDay(schedule, day, "graveyard")
 
 #simulatedAnnealing is like hill climb
 #except with a probablistic choice at the end
@@ -12,9 +61,11 @@ def simulatedAnnealing():
     print("Simulated Annealing: ")
 
 def main():
+    #take from args optionally?
     #do the thing
-    hillClimb()
-
+    schedule = hillClimb("sch2.txt")
+    schedule.toFile("new_schedule.txt")
+    print(schedule.value3())
 
     pass
 

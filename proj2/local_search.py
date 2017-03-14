1524c63367c4577d01798f9affd1a3c166bb57bb
linkvetern64 <jstand1@umbc.edu>
2017-03-14 01:23:25 -0400

    Simulated annealing math

--- proj2/local_search.py
+++ proj2/local_search.py
@@ -1,12 +1,9 @@
 from schedule import Schedule
 import copy
-
-
-
-
+import math
+import random
 
 def hillClimb(file):
-    print("Hill Climbing: ")
     schedule = tmp = Schedule(0,0)
     schedule.load(file)
     #For each day
@@ -47,26 +44,46 @@ def hillClimb(file):
         day.graveyard = tmp_g
     return schedule
 
-                #day.morning = getBestDay(schedule, day, "morning")
-        #getBestDay(schedule, day, "evening")
-        #getBestDay(schedule, day, "graveyard")
+def probability(curr, new, )
 
 #simulatedAnnealing is like hill climb
 #except with a probablistic choice at the end
-def simulatedAnnealing():
-    print("Simulated Annealing: ")
-    #while t > 0
-    #keep
-    #
-    #
-    #
-    #
-    #
+def simulatedAnnealing(file):
+    print(file)
+    schedule = Schedule(0, 0)
+    schedule.load(file)
+    T = 100
+    for day in schedule.schedule:
+        tmp_m = day.morning
+        tmp_e = day.evening
+        tmp_g = day.graveyard
+        best = schedule.value3()
+        rand_worker = random.randrange(0, schedule.num_workers)
+
+        day.morning = rand_worker
+        if schedule.value3() > best:
+            print("Test")
+
+        rand_worker = random.randrange(0, schedule.num_workers)
+        #do a deep copy of the schedule and make changes to see if the change is better
+        #run that value through a probablistic function
+        #pick a random shift and a random worker
+        #
+        #
+        #
+
+        day.morning = tmp_m
+        day.evening = tmp_e
+        day.graveyard = tmp_g
+    return schedule
 
 def main():
     #take from args optionally?
     #do the thing
-    schedule = hillClimb("sch2.txt")
+    #schedule = hillClimb("sch2.txt")
+
+    schedule = simulatedAnnealing("sch2.txt")
+
     schedule.toFile("new_schedule.txt")
     print(schedule.value3())
     #let TA know how to use program

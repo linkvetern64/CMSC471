c010d52499a465b11c123847254728362ffc0c6e
linkvetern64 <jstand1@umbc.edu>
2017-04-23 16:56:04 -0400

    Updated proj3 stubs

--- proj3/stubproj3.py
+++ proj3/stubproj3.py
@@ -7,6 +7,30 @@ Please do not change the signature of train() or classify(),
 or you will break the test suite.
 '''
 
+#the following are the values for each attibute in the global context so you can use them as needed
+work_class = ["Private", "Self-emp-not-inc", "Self-emp-inc", "Federal-gov", "Local-gov", "State-gov", "Without-pay", "Never-worked"]
+
+education = ["Bachelors", "Some-college", "11th", "HS-grad", "Prof-school", "Assoc-acdm", "Assoc-voc", "9th", "7th-8th", "12th", 
+             "Masters", "1st-4th", "10th", "Doctorate", "5th-6th", "Preschool"]
+
+marital = ["Married-civ-spouse", "Divorced", "Never-married", "Separated", "Widowed", "Married-spouse-absent", "Married-AF-spouse"]
+
+occupation = ["Tech-support", "Craft-repair", "Other-service", "Sales", "Exec-managerial", "Prof-specialty", "Handlers-cleaners", 
+              "Machine-op-inspct", "Adm-clerical", "Farming-fishing", "Transport-moving", "Priv-house-serv", "Protective-serv", 
+              "Armed-Forces"]
+
+relationship = ["Wife", "Own-child", "Husband", "Not-in-family", "Other-relative", "Unmarried"]
+
+race = ["White", "Asian-Pac-Islander", "Amer-Indian-Eskimo", "Other", "Black"]
+
+sex = ["Female", "Male"]
+
+native_country = ["United-States", "Cambodia", "England", "Puerto-Rico", "Canada", "Germany", "Outlying-US(Guam-USVI-etc)", 
+                  "India", "Japan", "Greece", "South", "China", "Cuba", "Iran", "Honduras", "Philippines", "Italy", "Poland", 
+                  "Jamaica", "Vietnam", "Mexico", "Portugal", "Ireland", "France", "Dominican-Republic", "Laos", "Ecuador", 
+                  "Taiwan", "Haiti", "Columbia", "Hungary", "Guatemala", "Nicaragua", "Scotland", "Thailand", "Yugoslavia", 
+                  "El-Salvador", "Trinadad&Tobago", "Peru", "Hong", "Holand-Netherlands"]
+
 def train(data, labels):
     """
     This function should train a decision tree classifier
@@ -33,40 +57,16 @@ def classify(x, model):
 def convert(data_list):
     """This function converts the categorical values of data_list into integers """
 
-    work_class = ["Private", "Self-emp-not-inc", "Self-emp-inc", "Federal-gov", "Local-gov", "State-gov", "Without-pay", "Never-worked"]
-
-    education = ["Bachelors", "Some-college", "11th", "HS-grad", "Prof-school", "Assoc-acdm", "Assoc-voc", "9th", "7th-8th", "12th", 
-                 "Masters", "1st-4th", "10th", "Doctorate", "5th-6th", "Preschool"]
-
-    marital = ["Married-civ-spouse", "Divorced", "Never-married", "Separated", "Widowed", "Married-spouse-absent", "Married-AF-spouse"]
-
-    occupation = ["Tech-support", "Craft-repair", "Other-service", "Sales", "Exec-managerial", "Prof-specialty", "Handlers-cleaners", 
-                  "Machine-op-inspct", "Adm-clerical", "Farming-fishing", "Transport-moving", "Priv-house-serv", "Protective-serv", 
-                  "Armed-Forces"]
-
-    relationship = ["Wife", "Own-child", "Husband", "Not-in-family", "Other-relative", "Unmarried"]
-
-    race = ["White", "Asian-Pac-Islander", "Amer-Indian-Eskimo", "Other", "Black"]
-
-    sex = ["Female", "Male"]
-
-    native_country = ["United-States", "Cambodia", "England", "Puerto-Rico", "Canada", "Germany", "Outlying-US(Guam-USVI-etc)", 
-                      "India", "Japan", "Greece", "South", "China", "Cuba", "Iran", "Honduras", "Philippines", "Italy", "Poland", 
-                      "Jamaica", "Vietnam", "Mexico", "Portugal", "Ireland", "France", "Dominican-Republic", "Laos", "Ecuador", 
-                      "Taiwan", "Haiti", "Columbia", "Hungary", "Guatemala", "Nicaragua", "Scotland", "Thailand", "Yugoslavia", 
-                      "El-Salvador", "Trinadad&Tobago", "Peru", "Hong", "Holand-Netherlands"]
-
     attributes = [work_class, education, marital, occupation, relationship, race, sex, native_country]
 
     converted_data = []
 
     for attribute_value in data_list:
-        prev_len = len(converted_data)
 
         for attribute_values in attributes:
+
             if attribute_value in attribute_values:
                 converted_data.append(attribute_values.index(attribute_value))
-        post_len = len(converted_data)
 
     return converted_data
     
